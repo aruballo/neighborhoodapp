04e03acb31c4f62c288f1ec34c6b11dc6ecaa7b6
Antonio <aruballo@uci.edu>
2015-05-12 00:33:22 -0700

    Modified init function of model object to load categories and sub categories

--- js/app.js
+++ js/app.js
@@ -2,20 +2,50 @@ var neighborhoodApp = neighborhoodApp || {};
 
 neighborhoodApp.model = {
 	init: function(){
+		var self = this;
+		this.fullCategoriesData;
 		
-	}
-	loadData: function(){
+		var request = neighborhoodApp.getxmlhttpObject();
+		request.open("GET", "js/yelpCategories.json", true);
+		request.send(null);
+		request.onreadystatechange = function() {
+			if ( request.readyState === 4 && request.status === 200) {
+				if(request.responseText){
+					self.fullCategoriesData = JSON.parse(request.responseText);
+					self.loadParentandSubCategories();
+				}
+			}			
+		};	
+
+	},
+	loadParentandSubCategories: function(){
+		this.parentCategories = [];
+		this.subCategories = [];
 		
-	}
-	
+		for(var i = 0; i < this.fullCategoriesData.length; i++){
+			var currentObject = this.fullCategoriesData[i];
+			if(currentObject.parents[0] === "null"){
+				this.parentCategories.push(currentObject);
+			}
+			else{
+				this.subCategories.push(currentObject);
+			}
+		}
+	},
 };
 
 neighborhoodApp.viewModel = {
 	init: function(){
+		var self = this;
+		
 		this.inputView = ko.observable("dropdowns");
 		this.dropdownsVisible = ko.observable(true);
 		this.searchbarVisible = ko.observable(false);
+		this.categories;
+		
 		neighborhoodApp.mapView.init();
+		neighborhoodApp.model.init();
+			
 	},
 	toggleInputView: function(){
 		if(this.inputView() == "dropdowns"){
@@ -30,7 +60,13 @@ neighborhoodApp.viewModel = {
 		//in this case its the checking of the radio button. Returning true allows the
 		//default action to occur
 		return true;	
-	}	
+	},
+	loadMarkers: function(){
+		
+	},
+	loadCategories: function(){
+		this.categories = ko.observableArray(neighborhoodApp.model.parentCategories);
+	}
 };
 
 neighborhoodApp.mapView = {
